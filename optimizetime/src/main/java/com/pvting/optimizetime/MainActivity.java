dbf45209d1bbc6b8439ab73fe5de5be1c70a1abd
pvting <pvting@gmail.com>
2022-09-25 23:34:49 +0800

    (feature):traceview+profile

--- optimizetime/src/main/java/com/pvting/optimizetime/MainActivity.java
+++ optimizetime/src/main/java/com/pvting/optimizetime/MainActivity.java
@@ -3,12 +3,36 @@ package com.pvting.optimizetime;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.Debug;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
+    String TAG = "MainActivity1111";
+    @Override
+    public void onAttachedToWindow() {
+        super.onAttachedToWindow();
+
+        Log.d(TAG, "onAttachedToWindow: ");
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Debug.startMethodTracing();
         setContentView(R.layout.activity_main);
+        Debug.stopMethodTracing();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+        Log.d(TAG, "onStop: ");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
     }
 }
